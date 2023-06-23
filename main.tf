ac1a7906656e2baa94f411acb7a2296eaf22f6c6
david randolph <david.randolph@hashicorp.com>
2023-06-23 17:04:10 -0500

    refactored code to please new aws module.

--- main.tf
+++ main.tf
@@ -22,6 +22,25 @@ module "networks"{
 }
 */
 
+##########################################
+## Set up a couple of things in AWS.  ####
+##########################################
+# Data source used to grab the TLS certificate for Terraform Cloud.
+#
+# https://registry.terraform.io/providers/hashicorp/tls/latest/docs/data-sources/certificate
+data "tls_certificate" "tfc_certificate" {
+  count = var.Use_AWS == true || var.Use_GCP == true ? 1 : 0
+  url = "https://${var.tfc_hostname}"
+}
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_openid_connect_provider
+resource "aws_iam_openid_connect_provider" "tfc_provider" {
+  count = var.Use_AWS == true ? 1 : 0
+  url             = data.tls_certificate.tfc_certificate[count.index].url
+  client_id_list  = [var.tfc_aws_audience]
+  thumbprint_list = [data.tls_certificate.tfc_certificate[count.index].certificates[0].sha1_fingerprint]
+}
+
+
 module "terraform-aws"{
   source = "./modules/tfc_workspace_env"
    
@@ -37,6 +56,12 @@ module "terraform-aws"{
   # Vault Stuff
   VAULT_ADDR =var.VAULT_ADDR
   VAULT_NAMESPACE = "terraform_workloads"
+
+  # AWS Stuff
+  enable_aws_dynamic_workspace_creds=true
+  aws_oidc_provider_arn = aws_iam_openid_connect_provider.tfc_provider[0].arn
+  aws_oidc_provider_client_id_list = aws_iam_openid_connect_provider.tfc_provider[0].client_id_list
+
 }
 
 module "consul-cluster"{
@@ -53,7 +78,7 @@ module "consul-cluster"{
   # vault stuff
   VAULT_ADDR =var.VAULT_ADDR
   VAULT_NAMESPACE = "terraform_workloads"
-  enable_aws_dynamic_workspace_creds=true
+  enable_aws_dynamic_workspace_creds=false
   # AWS Creds 
   #vault_dynamic_creds_master_user = data.tfe_outputs.core-network.values.vault_dynamic_creds_master_user.name
 }
@@ -83,4 +108,6 @@ module "terraform_gcp"{
   VAULT_ADDR =var.VAULT_ADDR
   VAULT_NAMESPACE = "terraform_workloads"
   HCP_Packer_RunTask_ID=var.HCP_Packer_RunTask_ID
+  enable_gcp_dynamic_workspace_creds=true
+  gcp_project_id="mystical-glass-360520"
 }
